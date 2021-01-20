5f37d3690fd19c6c33cd09908532d2d7083cd5d6
Miroslav Trnavský <miroslav.trnavsky@brackets.sk>
2021-01-20 13:51:42 +0100

    removed unused imports

--- src/Channels/SMSProvider.php
+++ src/Channels/SMSProvider.php
@@ -1,17 +1,13 @@
 <?php
 
-
 namespace Brackets\Verifications\Channels;
 
 use Brackets\Verifications\Channels\Contracts\EmailProviderInterface;
-use Brackets\Verifications\Models\Verifiable;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\View;
 
 abstract class SMSProvider implements EmailProviderInterface
 {
-    public function renderSMSMessage($code)
+    public function renderSMSMessage($code): string
     {
         return View::make("brackets/verifications::sms.verification-sms", ['code' => $code])->render();
     }
