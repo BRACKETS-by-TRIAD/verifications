e1de0999a37d1a8921a14578df4d9ef9746024b0
Miroslav Trnavský <miroslav.trnavsky@brackets.sk>
2021-01-28 13:59:54 +0100

    implemented graphql mutations for 2fa

---
+++ src/GraphQL/Mutations/LoginWithCredentials.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Verifications\GraphQL\Mutations;
+
+use Brackets\Verifications\Facades\Verification;
+use GraphQL\Error\Error;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+
+class LoginWithCredentials
+{
+    /**
+     * @param  null $_
+     * @param  array<string, mixed> $args
+     * @return string
+     * @throws Error
+     */
+    public function __invoke($_, array $args)
+    {
+        $guard = Auth::guard('admin');
+
+        if (!$guard->attempt($args)) {
+            throw new Error(__('Invalid credentials.'));
+        }
+
+        return (Verification::verify($args['action'], URL::previous()) instanceof RedirectResponse)
+            ? 'VERIFICATION_REQUIRED'
+            : $guard->user()->createToken('api')->plaintTextToken;
+    }
+}
---
+++ src/GraphQL/Mutations/VerifyWithCode.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Brackets\Verifications\GraphQL\Mutations;
+
+use Brackets\Verifications\Facades\Verification;
+use Illuminate\Support\Facades\Auth;
+
+class VerifyWithCode
+{
+    /**
+     * @param  null $_
+     * @param  array<string, mixed> $args
+     * @return string
+     * @throws Error
+     */
+    public function __invoke($_, array $args)
+    {
+        $guard = Auth::guard('admin');
+
+        return Verification::verifyCode($guard->user(), $args['action'], $args['code'])
+            ? $guard->user()->createToken('api')->plaintTextToken
+            : 'VERIFICATION_FAILED';
+    }
+}
