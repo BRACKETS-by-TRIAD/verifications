ee4a351a3886c16008cc949eb8e98b863d7c0458
Miroslav Trnavský <miroslav.trnavsky@brackets.sk>
2021-01-29 16:02:13 +0100

    removed js submit

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -109,11 +109,11 @@
                     </form>
                 </div>
                 <div class="card-footer">
-                    <small>@lang('brackets/verifications::verifications.didnt_receive_code')</small>
                     <form action="{{ \Illuminate\Support\Facades\URL::route('brackets/verifications/resend') }}" method="POST" role="form">
                         @csrf
                         <input type="hidden" name="action_name" value="{{ $action_name }}">
 
+                        <small>@lang('brackets/verifications::verifications.didnt_receive_code')</small>
                         <button type="submit" class="btn btn-outline-secondary pull-right" data-toggle="tooltip" title="{{ trans('brackets/verifications::verifications.send_new_code') }}">
                             <i class="icon-repeat"></i>
                         </button>
@@ -123,7 +123,6 @@
         </div>
     </div>
 </div>
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
