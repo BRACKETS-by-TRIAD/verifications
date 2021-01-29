611571b689bd377331b930d464a6dd920023dce1
Miroslav Trnavský <miroslav.trnavsky@brackets.sk>
2021-01-29 16:00:08 +0100

    removed js submit

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -110,9 +110,14 @@
                 </div>
                 <div class="card-footer">
                     <small>@lang('brackets/verifications::verifications.didnt_receive_code')</small>
-                    <button type="button" onclick="generateNewCode()" class="btn btn-outline-secondary pull-right" data-toggle="tooltip" title="{{ trans('brackets/verifications::verifications.send_new_code') }}">
-                        <i class="icon-repeat"></i>
-                    </button>
+                    <form action="{{ \Illuminate\Support\Facades\URL::route('brackets/verifications/resend') }}" method="POST" role="form">
+                        @csrf
+                        <input type="hidden" name="action_name" value="{{ $action_name }}">
+
+                        <button type="submit" class="btn btn-outline-secondary pull-right" data-toggle="tooltip" title="{{ trans('brackets/verifications::verifications.send_new_code') }}">
+                            <i class="icon-repeat"></i>
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -123,17 +128,5 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script type="text/javascript">
-    function generateNewCode() {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open("POST", '{{ \Illuminate\Support\Facades\URL::route('brackets/verifications/resend') }}', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('X-CSRF-TOKEN', $('meta[name="csrf-token"]').attr('content'));
-        xhr.send(JSON.stringify({
-            action_name: document.getElementsByName("action_name").value
-        }));
-    }
-</script>
 </body>
 </html>
